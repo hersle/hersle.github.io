e5b084503d86806bc191715d1e5d1282469b8c95
Herman Sletmoen <hermansletmoen@gmail.com>
2023-08-22 15:53:53 +0200

    Print pandoc help and extensions

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ for srcpath in *.md; do
 	dstpath="$(basename $srcpath .md).html"
 	echo "Building $dstpath:"
 	sed "s/href=\"$dstpath\"/href=\"$dstpath\" class=\"currentpage\"/" header.html > /tmp/header_current.html
+	pandoc --help
+	pandoc --list-extensions
 	pandoc $srcpath --metadata toc-title="Contents" --template=template.html -B /tmp/header_current.html -A footer.html --css=style.css --toc --mathjax --verbose -o $dstpath
 done
