13e32929389f900ac86821b23e783d8a4be2e405
Herman Sletmoen <hermansletmoen@gmail.com>
2023-08-22 15:50:28 +0200

    Make pandoc verbose

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ for srcpath in *.md; do
 	dstpath="$(basename $srcpath .md).html"
 	echo "Building $dstpath:"
 	sed "s/href=\"$dstpath\"/href=\"$dstpath\" class=\"currentpage\"/" header.html > /tmp/header_current.html
-	pandoc $srcpath --metadata toc-title="Contents" --template=template.html -B /tmp/header_current.html -A footer.html --css=style.css --toc --mathjax -o $dstpath
+	pandoc $srcpath --metadata toc-title="Contents" --template=template.html -B /tmp/header_current.html -A footer.html --css=style.css --toc --mathjax --verbose -o $dstpath
 done
