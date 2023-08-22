a9c1b598f62bf967bda011da1751f092a9a4e71c
Herman Sletmoen <hermansletmoen@gmail.com>
2023-08-22 16:00:12 +0200

    Explicitly specify MathJax CDN URL (Ubuntu/Debian overrides upstream behavior by defaulting to local installation)

--- build.sh
+++ build.sh
@@ -4,7 +4,5 @@ for srcpath in *.md; do
 	dstpath="$(basename $srcpath .md).html"
 	echo "Building $dstpath:"
 	sed "s/href=\"$dstpath\"/href=\"$dstpath\" class=\"currentpage\"/" header.html > /tmp/header_current.html
-	pandoc --help
-	pandoc --list-extensions
-	pandoc $srcpath --metadata toc-title="Contents" --template=template.html -B /tmp/header_current.html -A footer.html --css=style.css --toc --mathjax --verbose -o $dstpath
+	pandoc $srcpath --metadata toc-title="Contents" --template=template.html -B /tmp/header_current.html -A footer.html --css=style.css --toc --mathjax='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js' -o $dstpath
 done
