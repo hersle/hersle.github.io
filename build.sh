0c385fe61127449048d502e538aab520f53009f2
Herman Sletmoen <hermansletmoen@gmail.com>
2023-09-17 20:06:37 +0200

    Use latest Pandoc version

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ for srcpath in *.md; do
 	dstpath="$(basename $srcpath .md).html"
 	echo "Building $dstpath:"
 	sed "s/href=\"$dstpath\"/href=\"$dstpath\" class=\"currentpage\"/" header.html > /tmp/header_current.html
-	pandoc $srcpath --metadata toc-title="Contents" --template=template.html -B /tmp/header_current.html -A footer.html --css=style.css --toc --mathjax='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js' -o $dstpath
+	bin/pandoc $srcpath --metadata toc-title="Contents" --template=template.html -B /tmp/header_current.html -A footer.html --css=style.css --toc --mathjax='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js' -o $dstpath
 done
